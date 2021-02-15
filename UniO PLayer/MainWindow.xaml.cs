39063256cdd0b4a9b13495f122c38d9a73a28325
Kryštof Šuk <suk.krystof@gmail.com>
2021-02-15 06:01:00 +0100

    Basic UI, prep for sync

--- UniO PLayer/MainWindow.xaml.cs
+++ UniO PLayer/MainWindow.xaml.cs
@@ -46,22 +46,36 @@ namespace UniO_PLayer
                 }
 
                 if (MediaRight.FramePosition == MediaLeft.FramePosition)
-                    SyncLabel.Content = "IN SYNC";
+                    StatusLabel.Content = "IN SYNC";
                 if (MediaRight.FramePosition != MediaLeft.FramePosition)
                 {
-                    var diff = ((MediaRight.FramePosition - MediaLeft.FramePosition).Milliseconds / 1000.0 * MediaLeft.VideoFrameRate);
-                    if(diff < 1)
+                    var diff = ((MediaRight.FramePosition - MediaLeft.FramePosition).TotalMilliseconds / 1000.0 * MediaLeft.VideoFrameRate);
+                    if (Math.Abs(diff) < 1)
                     {
-                        SyncLabel.Content = "IN SYNC";
+                        StatusLabel.Content = "IN SYNC";
                     }
                     else
                     {
-                        SyncLabel.Content = "OFF BY: " + diff.ToString("0.0") + " Frame";
+                        StatusLabel.Content = "OFF BY: " + diff.ToString("0.0") + " Frame";
                     }
                 }
 
-                DLabel1.Content = MediaLeft.FramePosition;
-                DLabel2.Content = MediaRight.FramePosition;
+                double leftTime = ((double)((TimeSpan)MediaLeft.FramePosition).Ticks / (double)((TimeSpan)MediaLeft.NaturalDuration).Ticks);
+                double rightTime = ((double)((TimeSpan)MediaRight.FramePosition).Ticks / (double)((TimeSpan)MediaRight.NaturalDuration).Ticks);
+
+                if (double.IsNaN(leftTime))
+                    leftTime = 0;
+                if (double.IsNaN(rightTime))
+                    rightTime = 0;
+
+                if(!MediaLeft.IsSeeking)
+                    Left_Progress.Value = leftTime;
+                if (!MediaRight.IsSeeking)
+                    Right_Progress.Value = rightTime;
+
+
+                Left_Time.Content = MediaLeft.FramePosition.ToString(@"mm\:ss") + " (" + Math.Round((MediaLeft.FramePosition).TotalMilliseconds / 1000.0 * MediaLeft.VideoFrameRate) + ")";
+                Right_Time.Content = MediaRight.FramePosition.ToString(@"mm\:ss") + " (" + Math.Round((MediaRight.FramePosition).TotalMilliseconds / 1000.0 * MediaRight.VideoFrameRate) + ")";
             };
             infoTimer.Start();
         }
@@ -77,8 +91,10 @@ namespace UniO_PLayer
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            /*
             if (e.Key == Key.Space)
             {
+                Console.WriteLine("Space");
                 playing = !playing;
                 if (playing)
                 {
@@ -114,8 +130,146 @@ namespace UniO_PLayer
                 sync = true;
                 MediaLeft.StepForward();
                 MediaRight.StepForward();
-
             }
+            */
+        }
+
+        private void Left_Play_Click(object sender, RoutedEventArgs e)
+        {
+            MediaLeft.Play();
+            Left_Play.Visibility = Visibility.Hidden;
+            Left_Pause.Visibility = Visibility.Visible;
+        }
+
+        private void Left_Pause_Click(object sender, RoutedEventArgs e)
+        {
+            MediaLeft.Pause();
+            Left_Play.Visibility = Visibility.Visible;
+            Left_Pause.Visibility = Visibility.Hidden;
+        }
+
+        private void Left_Abort_Click(object sender, RoutedEventArgs e)
+        {
+            MediaLeft.Stop();
+        }
+
+        private void Left_Back_Click(object sender, RoutedEventArgs e)
+        {
+            MediaLeft.StepBackward();
+        }
+
+        private void Left_Forward_Click(object sender, RoutedEventArgs e)
+        {
+            MediaLeft.StepForward();
+        }
+
+        private void Right_Play_Click(object sender, RoutedEventArgs e)
+        {
+            MediaRight.Play();
+            Right_Play.Visibility = Visibility.Hidden;
+            Right_Pause.Visibility = Visibility.Visible;
+        }
+
+        private void Right_Pause_Click(object sender, RoutedEventArgs e)
+        {
+            MediaRight.Pause();
+            Right_Play.Visibility = Visibility.Visible;
+            Right_Pause.Visibility = Visibility.Hidden;
+        }
+
+        private void Right_Abort_Click(object sender, RoutedEventArgs e)
+        {
+            MediaRight.Stop();
+        }
+
+        private void Right_Back_Click(object sender, RoutedEventArgs e)
+        {
+            MediaRight.StepBackward();
+        }
+
+        private void Right_Forward_Click(object sender, RoutedEventArgs e)
+        {
+            MediaRight.StepForward();
+        }
+
+
+        bool Left_scrub = false;
+
+        private void Left_Progress_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Left_Progress.Value = SetProgressBarValue(e.GetPosition(Left_Progress).X, Left_Progress);
+            TimeSpan newTime = new TimeSpan((long)((double)(((TimeSpan)MediaLeft.NaturalDuration).Ticks) * Left_Progress.Value));
+
+            MediaLeft.ScrubbingEnabled = true;
+            MediaLeft.Position = newTime;
+            MediaLeft.ScrubbingEnabled = false;
+            Left_scrub = true;
+        }
+
+        private void Left_Progress_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Left_scrub)
+            {
+                Left_Progress.Value = SetProgressBarValue(e.GetPosition(Left_Progress).X, Left_Progress);
+                TimeSpan newTime = new TimeSpan((long)((double)(((TimeSpan)MediaLeft.NaturalDuration).Ticks) * Left_Progress.Value));
+
+                MediaLeft.ScrubbingEnabled = true;
+                MediaLeft.Position = newTime;
+                MediaLeft.ScrubbingEnabled = false;
+            }
+        }
+
+        private void Left_Progress_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Left_scrub = false;
+        }
+
+        private void Left_Progress_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Left_scrub = false;
+        }
+
+        private double SetProgressBarValue(double mousePosition, ProgressBar bar)
+        {
+            double ratio = mousePosition / bar.ActualWidth;
+            double ProgressBarValue = ratio * bar.Maximum;
+            return ProgressBarValue;
+        }
+
+        bool Right_scrub = false;
+
+        private void Right_Progress_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Right_Progress.Value = SetProgressBarValue(e.GetPosition(Right_Progress).X, Left_Progress);
+            TimeSpan newTime = new TimeSpan((long)((double)(((TimeSpan)MediaRight.NaturalDuration).Ticks) * Right_Progress.Value));
+
+            MediaRight.ScrubbingEnabled = true;
+            MediaRight.Position = newTime;
+            MediaRight.ScrubbingEnabled = false;
+            Right_scrub = true;
+        }
+
+        private void Right_Progress_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Right_scrub)
+            {
+                Right_Progress.Value = SetProgressBarValue(e.GetPosition(Right_Progress).X, Left_Progress);
+                TimeSpan newTime = new TimeSpan((long)((double)(((TimeSpan)MediaRight.NaturalDuration).Ticks) * Right_Progress.Value));
+
+                MediaRight.ScrubbingEnabled = true;
+                MediaRight.Position = newTime;
+                MediaRight.ScrubbingEnabled = false;
+            }
+        }
+
+        private void Right_Progress_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Right_scrub = false;
+        }
+
+        private void Right_Progress_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Right_scrub = false;
         }
     }
 }
